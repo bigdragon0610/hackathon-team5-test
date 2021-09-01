8a2e95aea1efe8061d23d4b8acd093a57ad25dfa
Naoki Nishiyama <naoki1010nissy@gmail.com>
2021-09-01 16:50:11 +0900

    てーすーとー

---
+++ copy.js
@@ -0,0 +1,57 @@
+function change_JPN(){
+No1.innerHTML="追告 A『シン・エヴァンゲリオン劇場版』絶賛公開中";
+No2.innerHTML="追告 B『シン・エヴァンゲリオン劇場版』絶賛公開中";
+No3.innerHTML="『シン・エヴァンゲリオン劇場版』 『Q:3.333』版予告・改２"
+No4.innerHTML="『シン・エヴァンゲリオン劇場版』本予告・改２";
+No5.innerHTML="シン・エヴァンゲリオン劇場版 本予告";
+No6.innerHTML="シン・エヴァンゲリオン劇場版 特報3";
+No7.innerHTML="シン・エヴァンゲリオン劇場版 特報2'";
+No8.innerHTML="シン・エヴァンゲリオン劇場版　特報2.5";
+No9.innerHTML="シン・エヴァンゲリオン劇場版　特報２";
+last1.innerHTML="シン・エヴァンゲリオン劇場版 特報１";
+No11.innerHTML="ヱヴァンゲリヲン新劇場版：Q";
+No11.className="lang_jpn";
+No12.innerHTML="ヱヴァンゲリヲン新劇場版：破";
+No12.className="lang_jpn";
+No13.innerHTML="ヱヴァンゲリヲン新劇場版：序";
+No13.className="lang_jpn";
+No14.innerHTML="新世紀エヴァンゲリオン";
+No14.className="lang_jpn"
+No15.innerHTML="「残酷な天使のテーゼ」MUSIC VIDEO（HDver.）/";
+No16.innerHTML="Zankoku na Tenshi no Te-ze“The Cruel Angel's Thesis”";
+No17.innerHTML="©カラー　©カラー／Project Eva.　©カラー／EVA製作委員会";
+last5.className= "small";
+img.className= "true";
+}
+
+function change_ENG(){
+No1.innerHTML="EVANGELION: 3.0+1.0 Trailer";
+No2.innerHTML="EVANGELION: 3.0+1.0 Trailer";
+No3.innerHTML="EVANGELION: 3.0+1.0 Q:3.333 ver. Trailer Re:2";
+No4.innerHTML="EVANGELION: 3.0+1.0 Trailer Re:2";
+No5.innerHTML="EVANGELION: 3.0+1.0 Trailer";
+No6.innerHTML="EVANGELION: 3.0+1.0 Teaser3";
+No7.innerHTML="EVANGELION: 3.0+1.0 Teaser2'";
+No8.innerHTML="EVANGELION: 3.0+1.0";
+No9.innerHTML="EVANGELION: 3.0+1.0";
+last1.innerHTML="EVANGELION:3.0+1.0 Teaser1";
+No11.innerHTML="EVANGELION:3.33";
+No11.className="lang_ENG";
+No12.innerHTML="EVANGELION:2.22";
+No12.className="lang_ENG";
+No13.innerHTML="EVANGELION:1.01";
+No13.className="lang_ENG";
+No14.innerHTML="NEON GENESIS EVANGELION";
+No14.className="lang_ENG";
+No15.innerHTML="Zankoku na Tenshi no Te-ze“The Cruel Angel's Thesis”";
+No16.innerHTML="MUSIC VIDEO（HDver.）";
+No17.innerHTML="©khara, inc. ©khara／Project Eva. ©khara／EVA製作委員会";
+last5.className= "nothing";
+img.className= "nothing";
+
+
+
+// new.innerHTML="EVANGELION:3.0+1.0"
+// new.className="new"
+
+}
